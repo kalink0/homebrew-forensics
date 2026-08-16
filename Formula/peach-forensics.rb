14c49891439bffb633587a174c986a66d7ac7901
kalink0 <kalinko@be-binary.de>
2026-08-16 08:04:57 +0200

    Refactor livecheck block in peach-forensics.rb

--- Formula/peach-forensics.rb
+++ Formula/peach-forensics.rb
@@ -3,6 +3,11 @@ class PeachForensics < Formula
   homepage "https://github.com/kalink0/peach-forensics"
   license "Apache-2.0"
 
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
   on_macos do
     on_arm do
       url "https://github.com/kalink0/peach-forensics/releases/download/v0.1.0/peach-macos-arm-v0.1.0.tar.gz"
@@ -13,11 +18,6 @@ class PeachForensics < Formula
       url "https://github.com/kalink0/peach-forensics/releases/download/v0.1.0/peach-macos-intel-v0.1.0.tar.gz"
       sha256 "1c4bc76690ce0256d2caef92c2331260e8c6e138a9e2f6b2803528f27a0f0b89"
     end
-  end
-
-  livecheck do
-    url :stable
-    strategy :github_latest
   end
 
   def install
