f661a85d2ae8de95ed1c62ba820e27cc22c076ba
kalink0 <kalinko@be-binary.de>
2026-08-24 18:26:03 +0200

    fix: match requried components order

--- Formula/peach-forensics.rb
+++ Formula/peach-forensics.rb
@@ -1,10 +1,9 @@
 class PeachForensics < Formula
   desc "Local-first forensic log viewer for DFIR timelines"
   homepage "https://github.com/kalink0/peach-forensics"
-  license "Apache-2.0"
-
   url "https://github.com/kalink0/peach-forensics/releases/download/v0.2.1/peach-macos-v0.2.1.tar.gz"
   sha256 "2d2be2829f5f4c247a00bfdeca5ec05affbef597492c1dee9cb75107121cf6d4"
+  license "Apache-2.0"
 
   livecheck do
     url :stable
