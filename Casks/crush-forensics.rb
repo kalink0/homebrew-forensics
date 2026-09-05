bb81fe06864038ff1c43e9ad441689c495cd18a6
Anka <runner@sjc22-bm204-a4bd4377-0b7f-4d6b-8fc0-1a0990935fda-12434E012086.local>
2026-09-05 20:10:22 +0000

    crush-forensics 0.18.0

--- Casks/crush-forensics.rb
+++ Casks/crush-forensics.rb
@@ -1,9 +1,9 @@
 cask "crush-forensics" do
   arch arm: "arm", intel: "intel"
 
-  version "0.17.0"
-  sha256 arm:   "6cf857420cf69c3c32439d90373e0d04fbf164cfe2415d9597e5bd1e47f8721e",
-         intel: "935e807f4aff8f0b767e18f5c5f7abb2975c1cbd5b0e2c2fab2dda3e51756988"
+  version "0.18.0"
+  sha256 arm:   "8eee0418aa4ef341a34ce8ae0ca511a01f7d3f9a9c0c64e9f49d2e1c8acd7863",
+         intel: "680c73428bc2629b52243d50e8826cc76aae69b292961851b14857f84c24d450"
 
   url "https://github.com/kalink0/crush-forensics/releases/download/v#{version}/crush-macos-#{arch}-v#{version}.zip",
       verified: "github.com/kalink0/crush-forensics/"
